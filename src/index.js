bf968a2a28846040f61839168c83a15a2b5e3d65
Baruf Kosgei <barufkosgei@gmail.com>
2023-03-27 19:33:26 -0500

    feat: add google analytics

--- src/index.js
+++ src/index.js
@@ -1,12 +1,19 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App";
-import { BrowserRouter } from "react-router-dom";
-import "./assets/scss/style.scss";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import { BrowserRouter } from 'react-router-dom';
+import './assets/scss/style.scss';
+import ReactGA from 'react-ga4';
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const root = ReactDOM.createRoot(document.getElementById('root'));
+ReactGA.initialize('G-K3YXGGYB7E');
 root.render(
   <BrowserRouter>
     <App />
   </BrowserRouter>
 );
+
+ReactGA.send({
+  hitType: 'pageview',
+  page: window.location.pathname,
+});
